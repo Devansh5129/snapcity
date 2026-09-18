76ad8ab9019e998d66a03d5c97b76296369a9273
Devansh Shukla <shukladevansh101@gmail.com>
2026-09-18 10:25:48 +0530

    Allow deployed frontend in CORS

--- server/server.js
+++ server/server.js
@@ -59,7 +59,8 @@ const allowedOrigins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://snapcity-lx28.onrender.com"
 ];
 
 app.use(
